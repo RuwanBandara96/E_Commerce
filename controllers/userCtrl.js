d49e8d85044e6380cd9576780f0b56eb7c2eac54
RuwanBandara96 <bandararuwan419@gmail.com>
2022-05-23 23:11:41 +0530

    init

---
+++ controllers/userCtrl.js
@@ -0,0 +1,21 @@
+const Users=require('../models/userModel')
+
+const userCtrl = {
+    register: async(req, res) => {
+        try {
+            const { name, email, password } = req.body;
+            const user = await Users.findOne({ email })
+            
+            if (user)
+                return res.status(400).json({ msg: "The email already exists" });
+            
+            if (password.length < 6)
+                return res.status(400).json({ msg: "The password could be more than 6 letters" });
+            
+        } catch (err) {
+            return res.status(500).json({ msg: err.message })
+        }
+    }
+}
+
+module.exports = userCtrl;
