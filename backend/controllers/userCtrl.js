2006c921014e538746bf412c8acb109cce4bb7c6
RuwanBandara96 <bandararuwan419@gmail.com>
2023-02-01 19:57:56 +0530

    Middleware created

--- backend/controllers/userCtrl.js
+++ backend/controllers/userCtrl.js
@@ -103,9 +103,11 @@ const userCtrl = {
     },
     getuser: async (req, res) => {
         try {
-            res.json(req.user);
+            const user = await Users.findById(req.user.id).select('-password');
+            if (!user) return res.status(400).json({ msg: "User does not exist" });
+            res.json(user);
         } catch (err) {
-            return res.status(500).json({ msg: err.message });
+            return res.status(500).json({ msg: err.message  });
         }
     }
 
